22088674c7fe49fd6d101b5b372cb79d5e20216b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-26 14:04:46 +0430

    cleaning

--- src/main/java/af/gov/anar/spm/sample/repository/SampleEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package af.gov.anar.spm.sample.repository;
-
-import af.gov.anar.spm.sample.model.SampleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SampleEntityRepository extends JpaRepository<SampleEntity, Long> {
-}
